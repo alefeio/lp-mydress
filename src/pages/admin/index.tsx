d12d60c48d747d0373c110d20c44b8558d3d7ae2
Alexandre Feio <alefeio@gmail.com>
2025-08-13 23:54:47 -0300

    admin - dash 6

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -26,13 +26,15 @@ export default function AdminPage() {
     //     return null;
     // }
 
-    return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Painel de Administração</h1>
-            <p className="text-lg">Bem-vindo, {session.user.name}!</p>
-            <div className="mt-8">
-                <AddDressForm />
-            </div>
-        </div>
-    );
+    // return (
+    //     <div className="container mx-auto p-4">
+    //         <h1 className="text-3xl font-bold mb-4">Painel de Administração</h1>
+    //         <p className="text-lg">Bem-vindo, {session.user.name}!</p>
+    //         <div className="mt-8">
+    //             <AddDressForm />
+    //         </div>
+    //     </div>
+    // );
+
+    return
 }
